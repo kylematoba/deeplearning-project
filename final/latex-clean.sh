108d061551244c605833779f4f45fc58557a61cc
Kyle Matoba <kylematoba@gmail.com>
2018-12-08 10:40:40 -0500

    Store work ahead of pulling in Jonathan's changes

---
+++ final/latex-clean.sh
@@ -0,0 +1,21 @@
+
+#!/bin/sh
+
+# adapted from: 
+# https://gist.github.com/dougalsutherland/266983/9c88f1ca1cf1420af03166dcfccb9cb10a21c110
+arg=${1:-.}
+exts="aux bbl blg brf fls idx ilg ind lof log lol lot nav out toc snm synctex.gz vrb"
+
+
+
+if [ -d $arg ]; then
+    for ext in $exts; do
+         rm -f $arg/*.$ext
+    done
+else
+    for ext in $exts; do
+         rm -f $arg.$ext
+    done
+fi
+
+
